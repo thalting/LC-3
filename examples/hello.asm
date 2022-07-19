6747309971b561320f2933106c9055e152101a13
halting <halting@riseup.net>
2022-07-19 19:53:38 -0300

    a bit of examples

---
+++ examples/hello.asm
@@ -0,0 +1,6 @@
+.ORIG x3000                        ; this is the address in memory where the program will be loaded
+LEA R0, HELLO_STR                  ; load the address of the HELLO_STR string into R0
+PUTs                               ; output the string pointed to by R0 to the console
+HALT                               ; halt the program
+HELLO_STR .STRINGZ "Hello World!"  ; store this string here in the program
+.END                               ; mark the end of the file
